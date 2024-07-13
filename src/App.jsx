ecbb9125e234295cde4b19ff176955c81f08401d
D_Zucho <danny.nico.dev@outlook.com>
2024-07-13 04:26:07 -0700

    initial commit + start header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ButtonGrtadient from './assets/svg/ButtonGradient'
+import './App.css'
+import Button from './components/Button'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+
+      <ButtonGrtadient />
+    </>
+  )
+}
+
+export default App
