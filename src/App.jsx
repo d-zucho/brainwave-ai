332c0256f2e28c3e2fe9fa5fc1242e9194c0898d
D_Zucho <danny.nico.dev@outlook.com>
2024-07-27 21:25:18 -0700

    add section wrapper component & start hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ButtonGrtadient from './assets/svg/ButtonGradient'
 import './App.css'
 import Button from './components/Button'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
+        <Hero />
       </div>
 
       <ButtonGrtadient />
