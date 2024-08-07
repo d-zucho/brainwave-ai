1ae6dc45b4791cf5f7daeeb936b7a2a5b7e596ba
D_Zucho <danny.nico.dev@outlook.com>
2024-08-07 14:27:43 -0700

    add Services section with bento box design

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
+import Services from './components/Services'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
 
       <ButtonGrtadient />
